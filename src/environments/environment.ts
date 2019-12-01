77110227624d88add7436d9484fa37368e2606c3
ogangel <angel_tkr_16@hotmail.com>
2019-12-01 12:24:26 -0500

    Subiendo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDaIYAZspekoBn0IV8_ER3aDzC7EndwWGg",
+    authDomain: "proyecto-guia-e1d17.firebaseapp.com",
+    databaseURL: "https://proyecto-guia-e1d17.firebaseio.com",
+    projectId: "proyecto-guia-e1d17",
+    storageBucket: "proyecto-guia-e1d17.appspot.com",
+    messagingSenderId: "253691097431",
+    appId: "1:253691097431:web:04044cf102d8e934de8e12",
+    measurementId: "G-HDZD2NXRCZ"
+  }
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
