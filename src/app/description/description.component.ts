1e34409c367c5022f9f8dbd22478d8c8f4bfeb25
ogangel <angel_tkr_16@hotmail.com>
2019-12-06 12:01:43 -0500

    Proyecto Angular

--- src/app/description/description.component.ts
+++ src/app/description/description.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ImageService } from 'src/app/shared/image.service';
 @Component({
   selector: 'app-description',
   templateUrl: './description.component.html',
   styleUrls: ['./description.component.css']
 })
+
 export class DescriptionComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  imageList : any[];
+  rowIndexArray: any[];
+    constructor(private service:ImageService) { }
+  
+    ngOnInit() {
+      this.service.imageDetailList.snapshotChanges().subscribe(
+        list => {
+          this.imageList = list.map(item => {return item.payload.val();});
+          this.rowIndexArray = Array.from(Array(Math.ceil(this.imageList.length+1 / 3)).keys());
+        }
+      );
+    }
+  
   }
-
-}
--- src/app/images/image/image.component.ts
+++ src/app/images/image/image.component.ts
@@ -11,13 +11,16 @@ import { ImageService } from 'src/app/shared/image.service';
 })
 export class ImageComponent implements OnInit {
   imgSrc : string;
+  imgSrc2 : string;
   selectedImage: any = null;
+  selectedImage2: any = null;
   isSubmitted:boolean;
 
   formTemplate = new FormGroup({
     caption : new FormControl('',Validators.required),
     category : new FormControl(''),
     imageUrl : new FormControl('',Validators.required),
+    imageUrl2 : new FormControl('')
   })
 
   constructor(private storage:AngularFireStorage, private service:ImageService) { }
@@ -36,23 +39,52 @@ export class ImageComponent implements OnInit {
       this.selectedImage = null;
     }
   }
+  showPreview2(event:any){
+    if(event.target.files && event.target.files[0]){
+      const reader = new  FileReader();
+      reader.onload = (e:any) => this.imgSrc2 = e.target.result;
+      reader.readAsDataURL(event.target.files[0]);  
+      this.selectedImage2 = event.target.files[0];
+    }else{
+      this.imgSrc2 =  '/assets/img/placeholder.png';
+      this.selectedImage2 = null;
+    }
+  }
   onSubmit(formValue){
     this.isSubmitted = true;
     if(this.formTemplate.valid){
       var filePath = `${formValue.category}/${this.selectedImage.name.split('.').slice(0,-1).join('.')}_${new Date().getTime()}`;
       const fileRef = this.storage.ref(filePath);
-      this.storage.upload(filePath,this.selectedImage).snapshotChanges().pipe(
+      this.storage.upload(filePath,this.selectedImage).snapshotChanges().pipe (
         finalize(()=>{
           fileRef.getDownloadURL().subscribe((url)=>{
-            formValue['imageUrl']=url;
+            formValue['imageUrl'] = url;
             this.service.insertImageDetails(formValue);
-            this.resetForm();
+
           })
         })
-      ).subscribe();
+      ).subscribe()
     }
+    if(this.formTemplate.valid){
+      var filePath = `${formValue.category}/${this.selectedImage2.name.split('.').slice(0,-1).join('.')}_${new Date().getTime()}`;
+      const fileRef = this.storage.ref(filePath);
+      this.storage.upload(filePath,this.selectedImage2).snapshotChanges().pipe (
+        finalize(()=>{
+          fileRef.getDownloadURL().subscribe((url)=>{
+            formValue['imageUrl2'] = url;
+            this.service.insertImageDetails(formValue);
+
+          })
+        })
+      ).subscribe()
+    }
+
+    
   }
   get formControls(){
+    return this.formTemplate['controls'];
+  }
+  get formControls2(){
     return this.formTemplate['controls'];
   }
   resetForm(){
@@ -60,10 +92,13 @@ export class ImageComponent implements OnInit {
     this.formTemplate.setValue({
       caption:'',
       imageUrl:'',
+      imageUrl2:'', 
       category:''
     });
     this.imgSrc = '/assets/img/placeholder.png';
+    this.imgSrc2 = '/assets/img/placeholder.png';
     this.selectedImage = null;
+    this.selectedImage2 = null;
     this.isSubmitted=false;
   }
 }
